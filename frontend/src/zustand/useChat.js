19ead68042c6b04dab8996545ac8b0d56f61f2da
msa-developer <shoaib9542186823@gmail.com>
2025-10-24 18:35:41 +0530

    added some proper layout to the ui in the frontend

--- frontend/src/zustand/useChat.js
+++ frontend/src/zustand/useChat.js
@@ -2,40 +2,38 @@ import toast from "react-hot-toast";
 import { create } from "zustand";
 import axiosInstance from "../lib/axios";
 
-const useChat = create((set, get) => ({
-  message: [],
-  partners: [],
+const useChat = create((set) => ({
   contacts: [],
-  tab: null,
+  messages: [],
+  partners: [],
+  currentTab: "chats",
+  selectedUser: null,
+  loading: false,
 
-  changeTab: (tab) => {
-    set({ tab: tab });
-  },
+  changeTab: (tab) => set({ currentTab: tab }),
+  setSelectedUser: (selectedUser) => set({ selectedUser: selectedUser }),
 
-  getMessages: async (id) => {
-    try {
-      const res = await axiosInstance.get(`/message/${id}`);
-      set({ message: res.data });
-    } catch (error) {
-      toast.error(error.response?.data?.message);
-    }
-  },
-
-  getContacts: async () => {
+  getAllContacts: async () => {
+    set({ loading: true });
     try {
       const res = await axiosInstance.get("/message/contacts");
       set({ contacts: res.data });
     } catch (error) {
       toast.error(error.response?.data?.message);
+    } finally {
+      set({ loading: false });
     }
   },
 
-  getPartners: async () => {
+  Partners: async () => {
+    set({ loading: true });
     try {
-      const res = await axiosInstance.get("/message/contacts");
+      const res = await axiosInstance.get("/message/partners");
       set({ partners: res.data });
     } catch (error) {
       toast.error(error.response?.data?.message);
+    } finally {
+      set({ loading: false });
     }
   },
 }));
