ad35f2ce77ef1e6d198b7d4e54d5da3a98003005
msa-developer <shoaib9542186823@gmail.com>
2025-11-05 10:39:47 +0530

    finished the part in the frontend

--- frontend/src/Pages/RegisterPage.jsx
+++ frontend/src/Pages/RegisterPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const RegisterPage = () => {
-  const { data, setData } = React.useState({
+  const [data, setData] = React.useState({
     fullName: "",
     email: "",
     password: "",
@@ -11,7 +11,55 @@ const RegisterPage = () => {
     setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
-  return <div className="bg-red-900 "></div>;
+  return (
+    <div className="grid place-content-center min-h-screen">
+      <div className="card w-96 bg-base-100 card-xl md:w-xl shadow-sm">
+        <div className="card-body">
+          <h1 className="card-title">Register Now</h1>
+
+          <h2 className="card-title">fullName</h2>
+          <p>
+            <input
+              type="text"
+              value={data.fullName}
+              onChange={handleChange}
+              placeholder="fullName..."
+              name="fullName"
+              className="input input-md md:input-lg w-full"
+            />
+          </p>
+
+          <h2 className="card-title">Email</h2>
+          <p>
+            <input
+              type="email"
+              value={data.email}
+              onChange={handleChange}
+              placeholder="Email..."
+              name="email"
+              className="input input-md md:input-lg w-full"
+            />
+          </p>
+
+          <h2 className="card-title">Password</h2>
+          <p>
+            <input
+              value={data.password}
+              onChange={handleChange}
+              type="password"
+              placeholder="password..."
+              name="password"
+              className="input input-md md:input-lg w-full"
+            />
+          </p>
+
+          <div className="card-actions w-full">
+            <button className="btn btn-primary w-full">Register</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
