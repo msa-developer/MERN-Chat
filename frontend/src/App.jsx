edd978a4e43a22a10ba187ba8fd6822e88fb876a
msa-developer <shoaib9542186823@gmail.com>
2025-11-09 14:53:56 +0530

    added proper landing Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,27 +9,27 @@ import LandingPage from "./Pages/LandingPage/LandingPage.jsx";
 const App = () => {
   const { authUser, checkAuth } = useAuth();
 
-  // React.useEffect(() => {
-  //   checkAuth();
-  // }, [checkAuth]);
+  React.useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
 
   return (
-    <main className="min-h-screen overflow-hidden" data-theme="garden">
+    <main className="min-h-screen" data-theme="garden">
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        {/* <Route */}
-        {/*   path={"/"} */}
-        {/*   element={authUser ? <ChatPage /> : <Navigate to="/register" />} */}
-        {/* /> */}
-        {/* <Route */}
-        {/*   path={"/login"} */}
-        {/*   element={!authUser ? <LoginPage /> : <Navigate to="/" />} */}
-        {/* /> */}
-        {/**/}
-        {/* <Route */}
-        {/*   path={"/register"} */}
-        {/*   element={!authUser ? <RegisterPage /> : <Navigate to="/" />} */}
-        {/* /> */}
+        <Route
+          path={"/chat"}
+          element={authUser ? <ChatPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path={"/login"}
+          element={!authUser ? <LoginPage /> : <Navigate to="/chat" />}
+        />
+
+        <Route
+          path={"/register"}
+          element={!authUser ? <RegisterPage /> : <Navigate to="/chat" />}
+        />
       </Routes>
     </main>
   );
--- frontend/src/Pages/LandingPage/LandingPage.jsx
+++ frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -1,18 +1,25 @@
+import { Github } from "lucide-react";
 import HeroSection from "./HeroSection";
+import { Link } from "react-router";
 
 const LandingPage = () => {
   return (
     <>
-      <nav className="p-3">
-        <div className="navbar bg-base-300 shadow-sm ">
-          <a
-            href="https://mern-chat-dnmr.onrender.com/"
-            className="btn btn-neutral text-xl"
+      <section className="p-3 sticky top-0">
+        <nav className="navbar flex gap-2 justify-around bg-base-300 shadow-sm">
+          <Link
+            to="/chat"
+            className="btn font-bold btn-neutral text-xs md:text-lg"
           >
             Get Started Here
+          </Link>
+          <a href="https://github.com/msa-developer/Mern-Chat">
+            <button className="btn hover:btn-info">
+              <Github />
+            </button>
           </a>
-        </div>
-      </nav>
+        </nav>
+      </section>
       <HeroSection />
     </>
   );
